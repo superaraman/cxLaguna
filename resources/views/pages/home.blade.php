1910256de2f1d0f3147df1c69f7642ca46d84109
john06 <john06@simplexi.com>
2018-09-24 07:11:37 +0800

    Added responsiveness for mobile

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,21 +8,21 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
-            <div class="carousel-item" style="height: 100vh;">
+            <div class="carousel-item active">
                 <img class="d-block w-100" src="img/carousel/sea.jpg" alt="Second slide">
                 <div class="carousel-caption">
                     <h1>Second slide</h1>
                     <p>This is the second slide.</p>
                 </div>
             </div>
-            <div class="carousel-item active" style="height: 100vh;">
+            <div class="carousel-item">
                 <img class="d-block w-100" src="img/carousel/sea2.jpg" alt="First slide">
                 <div class="carousel-caption">
                     <h1>Second slide</h1>
                     <p>This is the second slide.</p>
                 </div>
             </div>
-            <div class="carousel-item" style="height: 100vh;">
+            <div class="carousel-item">
                 <img class="d-block w-100" src="img/carousel/sea3.jpg" alt="Third slide">
                 <div class="carousel-caption">
                     <h1>Second slide</h1>
@@ -32,10 +32,33 @@
         </div>
     </div>
 
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-2 text-center">SAMPLE</h1>
+            <h1 class="display-4">HISTORY</h1>
+            <p class="lead">The LLDA was organized by virtue of Republic Act No. 4850 as a quasi-government agency with
+                regulatory and proprietary functions. Through Presidential Decree 813 in 1975, and Executive Order 927
+                in 1983, its powers and functions were further strengthened to include environmental protection and
+                jurisdiction over the lake basin’s surface water. In 1993, through Executive Order 149, the
+                administrative supervision over LLDA was transferred from the Office of the President to the
+                Department of Environment and Natural Resources (DENR).
+            </p>
+
+            <h1 class="display-4">HISTORY</h1>
+            <p class="lead">The LLDA was organized by virtue of Republic Act No. 4850 as a quasi-government agency with
+                regulatory and proprietary functions. Through Presidential Decree 813 in 1975, and Executive Order 927
+                in 1983, its powers and functions were further strengthened to include environmental protection and
+                jurisdiction over the lake basin’s surface water. In 1993, through Executive Order 149, the
+                administrative supervision over LLDA was transferred from the Office of the President to the
+                Department of Environment and Natural Resources (DENR).
+            </p>
+        </div>
+    </div>
+
     <div class="container home-card-group d-flex">
         <div class="col align-self-center">
-            <div class="row mb-5">
-                <div class="col col-lg-4">
+            <div class="row">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top" src="/img/card/card-image.jpg" alt="Card image cap">
@@ -44,7 +67,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col col-lg-4">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top" src="/img/card/card-image1.jpg" alt="Card image cap">
@@ -53,7 +76,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col col-lg-4">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top" src="/img/card/card-image2.jpg" alt="Card image cap">
@@ -64,7 +87,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col col-lg-4">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top mb-2" src="/img/card/card-image.jpg" alt="Card image cap">
@@ -73,7 +96,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col col-lg-4">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top" src="/img/card/card-image1.jpg" alt="Card image cap">
@@ -82,7 +105,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col col-lg-4">
+                <div class="col-12 col-lg-4 my-1 my-lg-2">
                     <div class="card">
                         <div class="card-body">
                             <img class="card-img-top" src="/img/card/card-image2.jpg" alt="Card image cap">
@@ -116,28 +139,5 @@
             administrative supervision over LLDA was transferred from the Office of the President to the
             Department of Environment and Natural Resources (DENR).
         </p>
-    </div>
-
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-2 text-center">SAMPLE</h1>
-            <h1 class="display-4">HISTORY</h1>
-            <p class="lead">The LLDA was organized by virtue of Republic Act No. 4850 as a quasi-government agency with
-                regulatory and proprietary functions. Through Presidential Decree 813 in 1975, and Executive Order 927
-                in 1983, its powers and functions were further strengthened to include environmental protection and
-                jurisdiction over the lake basin’s surface water. In 1993, through Executive Order 149, the
-                administrative supervision over LLDA was transferred from the Office of the President to the
-                Department of Environment and Natural Resources (DENR).
-            </p>
-
-            <h1 class="display-4">HISTORY</h1>
-            <p class="lead">The LLDA was organized by virtue of Republic Act No. 4850 as a quasi-government agency with
-                regulatory and proprietary functions. Through Presidential Decree 813 in 1975, and Executive Order 927
-                in 1983, its powers and functions were further strengthened to include environmental protection and
-                jurisdiction over the lake basin’s surface water. In 1993, through Executive Order 149, the
-                administrative supervision over LLDA was transferred from the Office of the President to the
-                Department of Environment and Natural Resources (DENR).
-            </p>
-        </div>
     </div>
 @stop
